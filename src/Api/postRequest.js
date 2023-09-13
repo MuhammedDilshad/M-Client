8936dfff2b7bb30ef2323d914093b5c90aa65c4a
dilshad <md.mdsar@gmail.com>
2023-09-13 23:51:05 +0530

    api upated

--- src/Api/postRequest.js
+++ src/Api/postRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({ baseURL: "https://m-backend-mzrt.onrender.com" });
+// const API = axios.create({ baseURL: "http://localhost:4000" });
 
 export const uploadPost = (data) => API.post("/post", data);
 export const uploadImage = (data) => API.post("/upload", data);
